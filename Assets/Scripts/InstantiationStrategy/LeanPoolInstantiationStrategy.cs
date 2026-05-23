922a7d9d0643b058c259252a572601cd2a90a20d
Oberonjr <mateibogdanbulancea@gmail.com>
2026-05-23 21:09:25 +0200

    Lean Pool Strategy

---
+++ Assets/Scripts/InstantiationStrategy/LeanPoolInstantiationStrategy.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Pool;
+
+[CreateAssetMenu(fileName = "LeanPool Instantiation Strategy", menuName = "Instantiation Strategies/Lean Pool Instantiation Strategy")]
+public class LeanPoolInstantiationStrategy : InstantiationStrategy
+{
+    
+    public override void InitializePrefabs(int numberOfPrefabs, GameObject prefab, List<GameObject> objects)
+    {
+       
+    }
+
+    public override GameObject CreatePrefab(GameObject prefab, Vector3 position, List<GameObject> objects, Transform parent = null)
+    {
+        GameObject obj = LeanPool.Spawn(prefab);
+        obj.transform.position = position;
+        obj.SetActive(true);
+        if(parent != null) obj.transform.SetParent(parent);
+        return obj;
+    }
+
+    public override void DestroyPrefab(GameObject obj)
+    {
+        LeanPool.Despawn(obj);
+    }
+    
+    public override void DestroyPrefabs(List<GameObject> objects)
+    {
+        LeanPool.DespawnAll();
+    }
+}
+
